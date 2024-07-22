2a68153f8afe455b6cca84077f62c70ac559db45
imiii-el <elmessaoudi.imane11@gmail.com>
2024-07-22 09:18:01 -0400

    message

---
+++ Scope/scope_lab.js
@@ -0,0 +1,7 @@
+function show(){
+    var functionVar = "I'm a block-scoped var";
+    let functionLet = "I'm a block-scoped let";
+    const functionConst = "I'm a block-scoped const"; 
+    console.log(functionVar);
+    }
+    show();
